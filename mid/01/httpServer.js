f6d79ff282dea1d2fa18e2a38938f00f456d92fd
qwerty0905 <qwerty0905557925@gmail.com>
2019-11-20 23:12:30 +0800

    add xxx

--- mid/01/httpServer.js
+++ mid/01/httpServer.js
@@ -1,15 +1,17 @@
-const http = require('http');
+const http = require('http');//宣告或引用套件
 
-const port = 3000, hostname = 'localhost'
+const port = 3000, hostname = 'localhost'//宣告字串
 
 const server = http.createServer((request, response) => {
   console.log('url:', request.url);
   console.log('  method:', request.method);
   console.log('  headers', request.headers);
-  response.statusCode = 200;
+  response.statusCode = 200;//回應碼200表示成功
+  //response.statusCode不是函數
   response.setHeader('Content-Type', 'text/plain');
-  response.write('Hello World\n');
-  response.end()
+  //設定標頭
+  response.write('Hello World\n');//內容
+  response.end()//表示主程式結束
 });
 
 server.listen(port, () => {
---
+++ mid/Readme.md
@@ -0,0 +1,5 @@
+期中考
+01-http{
+    
+}
+    01 koaserver-https://github.com/qwerty0905/ws108a/blob/master/mid/01/httpServer.js
